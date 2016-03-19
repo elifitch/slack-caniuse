6ba7f866c98cbc136416648151a6d6dd465e6a6f
eli <eli.fitch.prof@gmail.com>
2016-03-19 17:39:59 -0400

    error handling

--- src/database.controller.js
+++ src/database.controller.js
@@ -16,43 +16,41 @@ module.exports = (function() {
     //this does way too much
     mongoClient.connect(dbUrl, {
       promiseLibrary: Promise
-    }).then(establishBaseline);
+    }).then(establishBaseline).catch(function(err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    });
   }
 
   function establishBaseline(db) {
-    if (!db) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-      //HURRAY!! We are connected. :)
-      console.log('Connection established to', db.options.url);
-      const features = db.collection('features');
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', db.options.url);
+    const features = db.collection('features');
 
-      getFile('https://raw.githubusercontent.com/elifitch/caniuse/master/data.json').then(function(caniuse) {
-        const ciu = JSON.parse(caniuse);
+    getFile('https://raw.githubusercontent.com/elifitch/caniuse/master/data.json').then(function(caniuse) {
+      const ciu = JSON.parse(caniuse);
 
-        const cleanData = encodeDots(ciu.data);
-        const length = _.size(cleanData);
-        let counter = 0;
+      const cleanData = encodeDots(ciu.data);
+      const length = _.size(cleanData);
+      let counter = 0;
 
-        for (var property in cleanData) {
-          if (cleanData.hasOwnProperty(property)) {
-            let feature = {};
-            feature.name = property;
-            feature.data = cleanData[property];
-            
-            // updates feature in db if already exists, if not present, adds feature
-            features.update({name: property}, feature, {
-              upsert: true
-            }).then(function() {
-              counter ++;
-              if (counter === length) {
-                db.close();
-              }
-            });
-          }
+      for (var property in cleanData) {
+        if (cleanData.hasOwnProperty(property)) {
+          let feature = {};
+          feature.name = property;
+          feature.data = cleanData[property];
+          
+          // updates feature in db if already exists, if not present, adds feature
+          features.update({name: property}, feature, {
+            upsert: true
+          }).then(function() {
+            counter ++;
+            if (counter === length) {
+              db.close();
+            }
+          });
         }
-      })
-    }
+      }
+    })
   }
 
 
@@ -66,26 +64,26 @@ module.exports = (function() {
   }
 
   function encodeDots(obj) {
-      var output = {};
-      for (var i in obj) {
-          if (Object.prototype.toString.apply(obj[i]) === '[object Object]') {
-              output[i.replace(/\./g, 'U+FF0E')] = encodeDots(obj[i]);
-          } else {
-              output[i.replace(/\./g, 'U+FF0E')] = obj[i];
-          }
-      }
-      return output;
+    var output = {};
+    for (var i in obj) {
+        if (Object.prototype.toString.apply(obj[i]) === '[object Object]') {
+            output[i.replace(/\./g, 'U+FF0E')] = encodeDots(obj[i]);
+        } else {
+            output[i.replace(/\./g, 'U+FF0E')] = obj[i];
+        }
+    }
+    return output;
   }
   function decodeDots(obj) {
-      var output = {};
-      for (var i in obj) {
-          if (Object.prototype.toString.apply(obj[i]) === '[object Object]') {
-              output[i.replace(/U\+FF0E/g, '.')] = decodeDots(obj[i]);
-          } else {
-              output[i.replace(/U\+FF0E/g, '.')] = obj[i];
-          }
-      }
-      return output;
+    var output = {};
+    for (var i in obj) {
+        if (Object.prototype.toString.apply(obj[i]) === '[object Object]') {
+            output[i.replace(/U\+FF0E/g, '.')] = decodeDots(obj[i]);
+        } else {
+            output[i.replace(/U\+FF0E/g, '.')] = obj[i];
+        }
+    }
+    return output;
   }
 
   
